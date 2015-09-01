9f9ad4fc5e9501b3ce32c86b92cdb339f71d618d
Takekazu Omi <takekazuomi@gmail.com>
2015-09-02 06:53:21 +0900

    for debug

--- tsung/install_tsung.sh
+++ tsung/install_tsung.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 apt-get -y update
 
-# install Apache2
-apt-get -y install tsung
+# install tsung
+apt-get -y install emacs24-nox git tsung
+
+echo ************** env **************
+env
+
+echo ************** pwd **************
+pwd
+
+echo ************** ls home **************
+ls /home/
