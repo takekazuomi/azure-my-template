79f79700d6c8a7ea73b57e07b4197842d6e7d46d
Takekazu Omi <takekazuomi@gmail.com>
2015-09-04 04:11:02 +0900

    add sample setting and kernel setting

--- tsung/install_tsung.sh
+++ tsung/install_tsung.sh
@@ -2,13 +2,25 @@
 apt-get -y update
 
 # install tsung
-apt-get -y install emacs24-nox git tsung
+apt-get -y install emacs24-nox git tsung unzip
 
-echo ************** env **************
-env
+#
+cat << EOF >>  /etc/sysctl.conf
+# here http://tsung.erlang-projects.org/user_manual/faq.html#why-do-i-have-error-connect-emfile-errors
 
-echo ************** pwd **************
-pwd
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.ip_local_port_range = 1024 65000
+fs.file-max = 100000
 
-echo ************** ls home **************
-ls /home/
+EOF
+
+sysctl -p
+
+# add limits for user
+cat <<EOF >> /etc/security/limits.conf
+
+*        soft    nofile  100000
+*        hard    nofile  100000
+
+EOF
